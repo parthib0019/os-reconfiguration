012565a1d81ccb3797e04ddc699d9eda3cf365a0
Parthib_0019 <parthib2290352@gmail.com>
2025-09-03 10:23:59 +0530

    first commitment 3/9/25

---
+++ historyCollecter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# File to save user commands
+OUTPUT_FILE="UserCommand.txt"
+
+# Get the user's history file (default: ~/.bash_history)
+HIST_FILE="${HOME}/.bash_history"
+
+# Copy all commands from history file to OUTPUT_FILE
+cp "$HIST_FILE" "$OUTPUT_FILE"
+
+echo "User command history saved to $OUTPUT_FILE"
